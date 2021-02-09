4f2426e95b51d5294182f5d8974b74a3d921c7a7
sahithi28pothuri <74135534+sahithi28pothuri@users.noreply.github.com>
2021-02-09 15:07:46 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# class23-project
+
+**SUPPLY MISSION**
---
+++ sketch.js
@@ -0,0 +1,101 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	//loading images of helicopter and package body
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	//package sprite object created
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.scale=0.2
+
+	//helicopter sprite object created
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	//ground object created
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//physics engine body
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+	//creation of box
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  //using sprite objects to display physics bodies
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	//movement of helicopter using left, right arrow keys
+	if(keyCode === LEFT_ARROW){
+		helicopterSprite.x = helicopterSprite.x-5;
+	}
+
+	if(keyCode === RIGHT_ARROW){
+		helicopterSprite.x = helicopterSprite.x+5;
+	}
+
+	if(keyCode === DOWN_ARROW){
+		packageSprite.addImage(packageIMG)
+		Matter.Body.setStatic(packageBody,false);
+	}
+
+}
